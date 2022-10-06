d87a3e067aef2ead53b9e3540ee0b7e3aa8ed81b
Irene Ilalio <irenei@hawaii.edu>
2022-10-05 22:44:37 -1000

    Completed Hackwich 5 parts 1-8

--- HackwichFive/ViewController.swift
+++ HackwichFive/ViewController.swift
@@ -15,10 +15,13 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var bottomLabel: UILabel!
     
+    
+    @IBOutlet weak var buttonLabel: UIButton!
+    
     //Part 6: global type Integer name currentIndex assigned to integer 0
     var currentIndex = 0
     //declare global array assign to five items of type string of fave foods
-    var favoriteFoodsArray = ["Poke bowl", "Garlic Shrimp", "Laulau", "Ribeye Steak", "Stuffed Uhu"]
+    var favoriteFoodsArray = ["Poke bowl", "Garlic Shrimp", "Laulau", "Prime Rib", "Stuffed Uhu"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,12 +32,26 @@ class ViewController: UIViewController {
         
         //Part 7: display item at index [0] upon app launch
         bottomLabel.text = favoriteFoodsArray[currentIndex]
-        
-        
+    
         
         
     }
 
-
-}
-
+//Part 8: set up if/else statement to iterate through each item in array upon button pressed
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        
+        if self.currentIndex < favoriteFoodsArray.count
+        {
+            self.bottomLabel.text = favoriteFoodsArray[currentIndex]
+            buttonLabel.setTitle("Next", for: UIControl.State.normal)
+            currentIndex += 1
+        }
+        else
+        {
+            print("button has been disabled")
+            (buttonLabel!).isEnabled = false
+        }
+        
+        
+    }  }
